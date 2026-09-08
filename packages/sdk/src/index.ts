e09007da263652fb16a24c318c58f0dbeeac3e33
Om Santoshwar <omsantoshwar02@gmail.com>
2026-09-08 14:50:49 +0530

    refactor: restructure into server and sdk, remove incorrect packages approach

--- packages/sdk/src/index.ts
+++
@@ -1,9 +0,0 @@
-// Server-side exports (use in your backend/API routes)
-export { generateRpSignature, verifyProof } from './server'
-
-// Client-side exports (use in your frontend)
-export { getFaceGatePreset, getFaceGateWidgetProps } from './client'
-export type { FaceGateWidgetConfig } from './client'
-
-// Types
-export type { RpContext, EnrollResult, VerifyResult, FaceGateSession } from './types'
